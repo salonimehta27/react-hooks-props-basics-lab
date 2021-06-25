857bbd34efde1e288e31da074136e30ad7089aab
Saloni <salonimehta2127@gmail.com>
2021-06-25 16:22:55 -0400

    completed assignment

--- src/data/user.js
+++ src/data/user.js
@@ -1,11 +1,11 @@
 const user = {
-  name: "Liza",
+  name: "Saloni",
   city: "New York",
   bio: "I made this!",
   color: "firebrick",
   links: {
-    github: "https://github.com/liza",
-    linkedin: "https://www.linkedin.com/in/liza/",
+    github: "https://github.com/salonimehta27",
+    linkedin: "https://www.linkedin.com/in/saloni-mehta-486952149/",
   },
 };
 
